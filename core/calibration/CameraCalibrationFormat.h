facebookresearch/projectaria_tools

commit 439f7a3778d1ce4f190f18e3d256ca6e78b7f820

{Feature} Add support for printing MPS python objects
Summary:
* Add support for stringifying C++ objects that are pybinded by specializing `fmt::formatter` for each of these types.
* All the `fmt::formatter` specializations are in separate files (next to the file defining the data structure). This allows our users to consume the core C++ data structures without `fmt` dependency.
* To extend the support to python, we define the `__repr__` function pybind and that simply calls `fmt::to_string(my_cpp_object)` and returns the string representation.
* `fmt` is highly templatized and very efficient. The stringification will work explicitly and implicitly.

Reviewed By: SeaOtocinclus

Differential Revision:
D48277084

Privacy Context Container: L1124100

fbshipit-source-id: 8b1d010264c7e25855d79022eb330d8742324cb0

core/calibration/CameraCalibrationFormat.h
@@ -1,0 +1,46 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <fmt/format.h>
+#include <format/Format.h>
+#include "CameraCalibration.h"
+
+/*
+ * fmt::format() specialization for CameraCalibration
+ */
+template <>
+struct fmt::formatter<projectaria::tools::calibration::CameraCalibration> {
+  // No parse function needed
+
+  // Format the Point object
+  template <typename FormatContext>
+  auto format(
+      const projectaria::tools::calibration::CameraCalibration& camCalib,
+      FormatContext& ctx) {
+    return format_to(
+        ctx.out(),
+        "CameraCalibration(label: {}, model name: {}, principal point: {}, focal length: {}, projection params: {}, image size (w,h): {}, T_Device_Camera:{})",
+        camCalib.getLabel(),
+        camCalib.modelName(),
+        camCalib.getPrincipalPoint(),
+        camCalib.getFocalLengths(),
+        camCalib.projectionParams(),
+        camCalib.getImageSize(),
+        camCalib.getT_Device_Camera());
+  }
+};

core/calibration/CMakeLists.txt
@@ -20,13 +20,13 @@ add_subdirectory(utility)
 
 add_library(sensor_calibration
     SensorCalibration.cpp SensorCalibration.h
-    CameraCalibration.cpp CameraCalibration.h
-    ImuMagnetometerCalibration.cpp ImuMagnetometerCalibration.h
+    CameraCalibration.cpp CameraCalibration.h CameraCalibrationFormat.h
+    ImuMagnetometerCalibration.cpp ImuMagnetometerCalibration.h ImuMagnetometerCalibrationFormat.h
     BarometerCalibration.cpp BarometerCalibration.h
     MicrophoneCalibration.cpp MicrophoneCalibration.h
 )
 target_include_directories(sensor_calibration PUBLIC "../")
-target_link_libraries(sensor_calibration PUBLIC error_handler camera_projection vrs_logging calibration_distort)
+target_link_libraries(sensor_calibration PUBLIC format error_handler camera_projection vrs_logging calibration_distort)
 
 add_library(device_cad_extrinsics DeviceCadExtrinsics.cpp DeviceCadExtrinsics.h)
 target_include_directories(device_cad_extrinsics PUBLIC "../")

core/calibration/ImuMagnetometerCalibrationFormat.h
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <fmt/format.h>
+#include <format/Format.h>
+#include "ImuMagnetometerCalibration.h"
+
+/*
+ * fmt::format() specialization for IMUCalibration
+ */
+template <>
+struct fmt::formatter<projectaria::tools::calibration::ImuCalibration> {
+  // No parse function needed
+
+  // Format the ImuCalibrattion object
+  template <typename FormatContext>
+  auto format(const projectaria::tools::calibration::ImuCalibration& imuCalib, FormatContext& ctx) {
+    return format_to(
+        ctx.out(),
+        "ImuCalibration(label: {}, T_Device_Imu: {})",
+        imuCalib.getLabel(),
+        imuCalib.getT_Device_Imu());
+  }
+};

core/calibration/camera_projections/CMakeLists.txt
@@ -22,6 +22,6 @@ target_sources(camera_models INTERFACE Linear.h Spherical.h KannalaBrandtK3.h Fi
 target_link_libraries(camera_models INTERFACE common)
 target_include_directories(camera_models INTERFACE $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/../..>)
 
-add_library(camera_projection CameraProjection.cpp CameraProjection.h)
+add_library(camera_projection CameraProjection.cpp CameraProjection.h CameraProjectionFormat.h)
 target_include_directories(camera_projection PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/../..>)
-target_link_libraries(camera_projection PUBLIC camera_models Eigen3::Eigen Sophus::Sophus)
+target_link_libraries(camera_projection PUBLIC camera_models format Eigen3::Eigen Sophus::Sophus)

core/calibration/camera_projections/CameraProjectionFormat.h
@@ -1,0 +1,53 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <fmt/format.h>
+#include <string_view>
+#include "CameraProjection.h"
+
+/*
+ * fmt::format() specialization for ModelType
+ */
+template <>
+struct fmt::formatter<projectaria::tools::calibration::CameraProjection::ModelType>
+    : fmt::formatter<string_view> {
+  // No parse function needed
+
+  // Format the ModelType object
+  template <typename FormatContext>
+  auto format(
+      const projectaria::tools::calibration::CameraProjection::ModelType& modelType,
+      FormatContext& ctx) const {
+    using mt = projectaria::tools::calibration::CameraProjection::ModelType;
+    auto to_string = [&](const mt& value) {
+      switch (value) {
+        case mt::Linear:
+          return "Linear";
+        case mt::Spherical:
+          return "Spherical";
+        case mt::KannalaBrandtK3:
+          return "KannalaBrandtK3";
+        case mt::Fisheye624:
+          return "Fisheye624";
+        default:
+          return "Unknown";
+      }
+    };
+    return formatter<string_view>::format(to_string(modelType), ctx);
+  }
+};

core/format/CMakeLists.txt
@@ -12,12 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-add_subdirectory(format)
-add_subdirectory(image)
-add_subdirectory(data_provider)
-add_subdirectory(calibration)
-add_subdirectory(mps)
-
-if(BUILD_PYTHON_BINDINGS)
-    add_subdirectory(python)
-endif(BUILD_PYTHON_BINDINGS)
+add_library(format INTERFACE)
+target_include_directories(format INTERFACE .)

core/format/Format.h
@@ -1,0 +1,87 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <fmt/format.h>
+#include <sophus/se3.hpp>
+
+/*
+ * fmt::format() specialization for Eigen Matrix
+ */
+template <typename SCALAR, int ROWS, int COLS, int Options, int MaxRows, int MaxCols>
+struct fmt::formatter<Eigen::Matrix<SCALAR, ROWS, COLS, Options, MaxRows, MaxCols>> {
+  // Need to define this function ourselves for partial specialization
+  constexpr auto parse(format_parse_context& ctx) -> format_parse_context::iterator {
+    return ctx.begin();
+  }
+
+  // Format the Eigen::Matrix object
+  template <typename FormatContext>
+  auto format(
+      const Eigen::Matrix<SCALAR, ROWS, COLS, Options, MaxRows, MaxCols>& mat,
+      FormatContext& ctx) {
+    std::stringstream ss;
+    if constexpr (COLS == 1) {
+      ss << mat.format(kVectorFormat);
+    } else {
+      ss << mat.format(kMatrixFormat);
+    }
+
+    return format_to(ctx.out(), "{}", ss.str());
+  }
+
+  const Eigen::IOFormat kMatrixFormat = Eigen::IOFormat(
+      Eigen::StreamPrecision,
+      Eigen::DontAlignCols, // flags
+      ", ", // coeffSeparator
+      ", ", // rowSeparator
+      "[", // rowPrefix
+      "]", // rowSuffix
+      "[", // matPrefix
+      "]"); // matSuffix;
+
+  // When printing Vectors, don't use the row prefix/suffix
+  const Eigen::IOFormat kVectorFormat = Eigen::IOFormat(
+      Eigen::StreamPrecision,
+      Eigen::DontAlignCols, // flags
+      ", ", // coeffSeparator
+      ", ", // rowSeparator
+      "", // rowPrefix
+      "", // rowSuffix
+      "[", // matPrefix
+      "]"); // matSuffix
+};
+
+/*
+ * fmt::format() specialization for Sophus SE3
+ */
+template <typename SCALAR>
+struct fmt::formatter<Sophus::SE3<SCALAR>> {
+  // Default parse implementation
+  auto parse(format_parse_context& ctx) {
+    return ctx.begin();
+  }
+
+  // Format the Sophus::SE3 object
+  template <typename FormatContext>
+  auto format(const Sophus::SE3<SCALAR>& se3, FormatContext& ctx) {
+    return format_to(
+        ctx.out(),
+        "(translation:{}, quaternion(x,y,z,w):{})",
+        se3.translation(),
+        se3.unit_quaternion().coeffs());
+  }
+};

core/mps/CMakeLists.txt
@@ -17,7 +17,7 @@ find_package(Boost REQUIRED COMPONENTS iostreams)
 add_library(compressed_istream CompressedIStream.cpp CompressedIStream.h)
 target_link_libraries(compressed_istream PUBLIC Boost::iostreams)
 
-add_library(eye_gaze EyeGaze.h EyeGazeReader.cpp EyeGazeReader.h)
+add_library(eye_gaze EyeGaze.h EyeGazeReader.cpp EyeGazeFormat.h EyeGazeReader.h)
 target_link_libraries(eye_gaze PUBLIC device_calibration_json)
 add_dependencies(eye_gaze fast-cpp-csv-parser)
 target_include_directories(eye_gaze
@@ -27,14 +27,19 @@ target_include_directories(eye_gaze
 
 add_library(mps
     OnlineCalibration.h
+    OnlineCalibrationFormat.h
     onlineCalibrationReader.h onlineCalibrationReader.cpp
     GlobalPointCloud.h
+    GlobalPointCloudFormat.h
     GlobalPointCloudReader.h GlobalPointCloudReader.cpp
     PointObservation.h
+    PointObservationFormat.h
     PointObservationReader.h PointObservationReader.cpp
     Trajectory.h
+    TrajectoryFormat.h
     TrajectoryReaders.h TrajectoryReaders.cpp
     StaticCameraCalibration.h
+    StaticCameraCalibrationFormat.h
     StaticCameraCalibrationReader.h StaticCameraCalibrationReader.cpp
 )
 target_link_libraries(mps
@@ -42,6 +47,7 @@ target_link_libraries(mps
         compressed_istream
         device_calibration_json
         eye_gaze
+        format
         Sophus::Sophus)
 add_dependencies(mps fast-cpp-csv-parser)
 target_include_directories(mps

commit ffbf2e069bc0687c57788f837bf6f3fa00067761

{BugFix}: Fix the build error in python bindings by including the missing header
Summary:
[Core]{BugFix}: Include the missing header to fix the build error in python bindings
Fix build error introduced in 439f7a3778d1ce4f190f18e3d256ca6e78b7f820

Reviewed By: SeaOtocinclus

Differential Revision: D48402668

fbshipit-source-id: bfbd06a7bf45c0f1aed19d3079b1a700c3b1fc58

core/calibration/CameraCalibrationFormat.h
@@ -19,6 +19,7 @@
 #include <fmt/format.h>
 #include <format/Format.h>
 #include "CameraCalibration.h"
+#include "camera_projections/CameraProjectionFormat.h"
 
 /*
  * fmt::format() specialization for CameraCalibration