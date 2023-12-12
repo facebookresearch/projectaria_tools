dc8bcd486c5315208a29797ada4bb14efe534a4a
Pierre Moulon <pierrem@meta.com>
2023-12-11 23:22:22 -0800

    {Feature} MPS - Static Camera Calibration - Unit test
    Summary:
    Add unit test for StaticCameraCalibration
    
    ```
    make -j
    
     ./core/mps/test/mps_TestStaticCameraCalibration
    Running main() from /tmp/googletest-20230121-4507-16cgq72/googletest-1.13.0/googletest/src/gtest_main.cc
    [==========] Running 2 tests from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 2 tests from mps_static_camera_calibration
    [ RUN      ] mps_static_camera_calibration.valid_file
    Loaded #StaticCameraCalibration data: 1
    [       OK ] mps_static_camera_calibration.valid_file (6 ms)
    [ RUN      ] mps_static_camera_calibration.invalid_file
    Failed to parse StaticCameraCalibration file: Can not open file "" because "No such file or directory".
    [       OK ] mps_static_camera_calibration.invalid_file (0 ms)
    [----------] 2 tests from mps_static_camera_calibration (6 ms total)
    
    [----------] Global test environment tear-down
    [==========] 2 tests from 1 test suite ran. (6 ms total)
    [  PASSED  ] 2 tests.
    ```
    
    Reviewed By: chpeng-fb
    
    Differential Revision: D51990644
    
    fbshipit-source-id: 52694d9ad322d63aa97f07021eac684f6e2f1f8c

--- core/mps/StaticCameraCalibrationReader.cpp
+++ core/mps/StaticCameraCalibrationReader.cpp
@@ -37,72 +37,74 @@ constexpr std::array<const char*, 22> kStaticCameraCalibrationHeader = {
 
 StaticCameraCalibrations readStaticCameraCalibrations(const std::string& fileName) {
   StaticCameraCalibrations poses;
+  try {
+    io::CSVReader<kStaticCameraCalibrationHeader.size()> csv(fileName);
+    // Read in the CSV header
+    // allow extra column for future-proof forward compatibility
+    const auto readHeader = [&](auto&&... args) {
+      csv.read_header(io::ignore_extra_column, args...);
+    };
+    std::apply(readHeader, kStaticCameraCalibrationHeader);
 
-  io::CSVReader<kStaticCameraCalibrationHeader.size()> csv(fileName);
-  // Read in the CSV header
-  // allow extra column for future-proof forward compatibility
-  const auto readHeader = [&](auto&&... args) {
-    csv.read_header(io::ignore_extra_column, args...);
-  };
-  std::apply(readHeader, kStaticCameraCalibrationHeader);
+    std::string cam_uid;
+    std::string graph_uid;
+    Eigen::Vector3d t_world_cam;
+    Eigen::Quaterniond q_world_cam;
+    std::string intrinsics_type;
+    int width = 0, height = 0;
+    Eigen::Matrix<float, 8, 1> intrinsics;
+    int start_frame_idx = 0, end_frame_idx = 0;
 
-  std::string cam_uid;
-  std::string graph_uid;
-  Eigen::Vector3d t_world_cam;
-  Eigen::Quaterniond q_world_cam;
-  std::string intrinsics_type;
-  int width, height;
-  Eigen::Matrix<float, 8, 1> intrinsics;
-  int start_frame_idx, end_frame_idx;
+    while (csv.read_row(
+        cam_uid,
+        graph_uid,
+        t_world_cam.x(),
+        t_world_cam.y(),
+        t_world_cam.z(),
+        q_world_cam.x(),
+        q_world_cam.y(),
+        q_world_cam.z(),
+        q_world_cam.w(),
+        width,
+        height,
+        intrinsics_type,
+        intrinsics[0],
+        intrinsics[1],
+        intrinsics[2],
+        intrinsics[3],
+        intrinsics[4],
+        intrinsics[5],
+        intrinsics[6],
+        intrinsics[7],
+        start_frame_idx,
+        end_frame_idx)) {
+      auto& pose = poses.emplace_back();
+      pose.cameraUid = cam_uid;
+      pose.graphUid = graph_uid;
+      pose.T_world_cam = Sophus::SE3d(q_world_cam, t_world_cam);
+      pose.width = width;
+      pose.height = height;
 
-  while (csv.read_row(
-      cam_uid,
-      graph_uid,
-      t_world_cam.x(),
-      t_world_cam.y(),
-      t_world_cam.z(),
-      q_world_cam.x(),
-      q_world_cam.y(),
-      q_world_cam.z(),
-      q_world_cam.w(),
-      width,
-      height,
-      intrinsics_type,
-      intrinsics[0],
-      intrinsics[1],
-      intrinsics[2],
-      intrinsics[3],
-      intrinsics[4],
-      intrinsics[5],
-      intrinsics[6],
-      intrinsics[7],
-      start_frame_idx,
-      end_frame_idx)) {
-    auto& pose = poses.emplace_back();
-    pose.cameraUid = cam_uid;
-    pose.graphUid = graph_uid;
-    pose.T_world_cam = Sophus::SE3d(q_world_cam, t_world_cam);
-    pose.width = width;
-    pose.height = height;
+      XR_CHECK_EQ(intrinsics_type, "KANNALABRANDTK3", "only KB3 type is supported today");
+      pose.intrinsicsType = intrinsics_type;
+      pose.intrinsics = intrinsics;
 
-    XR_CHECK_EQ(intrinsics_type, "KANNALABRANDTK3", "only KB3 type is supported today");
-    pose.intrinsicsType = intrinsics_type;
-    pose.intrinsics = intrinsics;
-
-    XR_CHECK(
-        (start_frame_idx == -1 && end_frame_idx == -1) ||
-            (start_frame_idx >= 0 && end_frame_idx >= 0 && start_frame_idx <= end_frame_idx),
-        "start and end frame indices are invalid");
-    if (start_frame_idx >= 0) {
-      pose.startFrameIdx = start_frame_idx;
+      XR_CHECK(
+          (start_frame_idx == -1 && end_frame_idx == -1) ||
+              (start_frame_idx >= 0 && end_frame_idx >= 0 && start_frame_idx <= end_frame_idx),
+          "start and end frame indices are invalid");
+      if (start_frame_idx >= 0) {
+        pose.startFrameIdx = start_frame_idx;
+      }
+      if (end_frame_idx >= 0) {
+        pose.endFrameIdx = end_frame_idx;
+      }
     }
-    if (end_frame_idx >= 0) {
-      pose.endFrameIdx = end_frame_idx;
-    }
+
+    std::cout << "Loaded #StaticCameraCalibration data: " << poses.size() << std::endl;
+  } catch (std::exception& e) {
+    std::cerr << "Failed to parse StaticCameraCalibration file: " << e.what() << std::endl;
   }
-
-  std::cout << "Loaded #StaticCameraCalibration data: " << poses.size() << std::endl;
-
   return poses;
 }
 
---
+++ core/mps/test/TestStaticCameraCalibration.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <mps/StaticCameraCalibrationReader.h>
+
+#include <filesystem>
+
+using namespace projectaria::tools::mps;
+
+static const std::string StaticCameraCalibrationFileTemplate =
+    R"(cam_uid,graph_uid,tx_world_cam,ty_world_cam,tz_world_cam,qx_world_cam,qy_world_cam,qz_world_cam,qw_world_cam,image_width,image_height,intrinsics_type,intrinsics_0,intrinsics_1,intrinsics_2,intrinsics_3,intrinsics_4,intrinsics_5,intrinsics_6,intrinsics_7,start_frame_idx,end_frame_idx
+ cam01,40084458-f219-16ca-0c3c-8bd94ee06e6c,1.691723,0.545741,-0.116511,-0.437056,-0.661141,0.501553,0.346871,3840,2160,KANNALABRANDTK3,1761.275757,1761.275757,1920.000000,1080.000000,0.036902,0.057131,-0.057154,0.018458,-1,-1)";
+
+TEST(mps_static_camera_calibration, valid_file) {
+  const std::string staticCameraCalibrationFilename = "static_camera_calibration.csv";
+  std::ofstream file(staticCameraCalibrationFilename);
+  file << StaticCameraCalibrationFileTemplate;
+  file.close();
+  const auto staticCalibrations = readStaticCameraCalibrations(staticCameraCalibrationFilename);
+  EXPECT_FALSE(staticCalibrations.empty());
+  std::filesystem::remove(staticCameraCalibrationFilename);
+}
+
+TEST(mps_static_camera_calibration, invalid_file) {
+  const auto staticCalibrations = readStaticCameraCalibrations("");
+  EXPECT_TRUE(staticCalibrations.empty());
+}
