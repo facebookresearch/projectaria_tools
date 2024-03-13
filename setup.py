1f22de29962e2911551bfa2fd9b00b3f5ffad96b
Pierre Moulon <pierrem@meta.com>
2024-03-13 14:16:05 -0700

    {Build} Introducing Pixi for build reproducibility
    Summary:
    This diff introduces the usage of `pixi` to ease local build and build on GitHub actions.
    
    Pixi is a package manager used to ease reproducibility for build and dependency gathering on multiple platforms.
    
    Testing native or modified code can now rely on running a single command line.
    
    How to use it?
    
    ```
    # Install pixi
    curl -fsSL https://pixi.sh/install.sh | bash
    
    # Compile Core C++ and test it
    pixi run run_c
    
    # Compile Python, install it and test it
    pixi run run_python
    ```
    
    Reviewed By: PiotrBrzyski
    
    Differential Revision: D51244940
    
    fbshipit-source-id: c4f0992b3bb2d59a927b852a51f2da4b7170fb93

--- setup.py
+++ setup.py
@@ -57,10 +57,6 @@ class CMakeBuild(build_ext):
         debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
         cfg = "Debug" if debug else "Release"
 
-        # CMake lets you override the generator - we need to check this.
-        # Can be set with Conda-Build, for example.
-        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
-
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
@@ -89,55 +85,16 @@ class CMakeBuild(build_ext):
             # Therefore, we delete the PYTHONPATH variable.
             del os.environ["PYTHONPATH"]
 
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
         if self.compiler.compiler_type != "msvc":
-            # Using Ninja-build since it a) is available as a wheel and b)
-            # multithreaded automatically. MSVC would require all variables be
-            # exported for Ninja to pick it up, which is a little tricky to do.
-            # Users can override the generator with CMAKE_GENERATOR in CMake
-            # 3.15+.
-            if not cmake_generator:
-                try:
-                    import ninja  # noqa: F401
-
-                    cmake_args += ["-GNinja"]
-                except ImportError:
-                    pass
-
-        else:
-
-            # Single config generators are handled "normally"
-            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
-
-            # CMake allows an arch-in-generator style for backward compatibility
-            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
-
-            # Specify the arch if using MSVC generator, but only if it doesn't
-            # contain a backward-compatibility arch spec already in the
-            # generator name.
-            if not single_config and not contains_arch:
-                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
-
-            # Multi-config generators have a different way to specify configs
-            if not single_config:
-                cmake_args += [
-                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
-                ]
-                build_args += ["--config", cfg]
+            if "Ninja" in cmake_generator:
+                cmake_args += ["-GNinja"]
 
         if sys.platform.startswith("darwin"):
             # Cross-compile support for macOS - respect ARCHFLAGS if set
             archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
             if archs:
                 cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
-
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call, not supported by pip or PyPA-build.
-            if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
-                build_args += [f"-j{self.parallel}"]
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
