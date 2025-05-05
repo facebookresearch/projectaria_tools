a532058ebfecb6a64473798e6ea5f4760042165f
Jingming Dong <djingming@meta.com>
2025-05-05 15:32:21 -0700

    {Feature} Core - Introduce a new hand tracking result struct for hand landmarks, wrist transform and wrist and palm normals
    Summary:
    Introduce a new structure `HandTrackingResult` as the future container for hand tracking outcome. This class will replace `WristAndPalmPose` in the future, and is recommended to be used for hand tracking result.
    
    We expose the following fields in this new struct
    * Hand confidence. This is the same as what's already available in WristAndPalm struct.
    * Hand landmarks (`Landmarks`) with their meaning specified in enum class `HandLamark` and the connectivity among them in `kHandJointConnections`. A total of 21 landmarks are provided.
    * Wrist transform (`T_Device_Wrist`). The rotation and position of the local reference frame of hand at wrist joint. This could be useful for users to define points and compute other quantities in local hand (wrist) space, and convert to device space.
    * Wrist and palm normal. This is the same as what's already available in WristAndPalm struct.
    
    Wrist and palm information can be deduced by the new landmark positions indexed by `HandLamark::WRIST` and `HandLamark::PALM_CENTER`. We also provide two helper functions `getWristPositionInDevice` and `getPalmPositionInDevice` to make transition to this new struct easier.
    
    Reviewed By: YLouWashU
    
    Differential Revision: D73066209
    
    fbshipit-source-id: 297c7f5fde150d6a27eca5bc6b23b131154925fb

--- core/mps/HandTracking.h
+++ core/mps/HandTracking.h
@@ -20,6 +20,7 @@
 #include <optional>
 #include <vector>
 
+#include <sophus/se3.hpp>
 #include <Eigen/Core>
 
 namespace projectaria::tools::mps {
@@ -29,31 +30,131 @@ namespace projectaria::tools::mps {
  */
 enum class HANDEDNESS { LEFT = 0, RIGHT = 1 };
 
+// A enum class to represent the type of hand landmarks
+enum class HandLandmark : uint8_t {
+  THUMB_FINGERTIP = 0,
+  INDEX_FINGERTIP = 1,
+  MIDDLE_FINGERTIP = 2,
+  RING_FINGERTIP = 3,
+  PINKY_FINGERTIP = 4,
+
+  WRIST = 5,
+
+  THUMB_INTERMEDIATE = 6,
+  THUMB_DISTAL = 7,
+
+  INDEX_PROXIMAL = 8,
+  INDEX_INTERMEDIATE = 9,
+  INDEX_DISTAL = 10,
+
+  MIDDLE_PROXIMAL = 11,
+  MIDDLE_INTERMEDIATE = 12,
+  MIDDLE_DISTAL = 13,
+
+  RING_PROXIMAL = 14,
+  RING_INTERMEDIATE = 15,
+  RING_DISTAL = 16,
+
+  PINKY_PROXIMAL = 17,
+  PINKY_INTERMEDIATE = 18,
+  PINKY_DISTAL = 19,
+
+  PALM_CENTER = 20,
+
+  NUM_LANDMARKS = 21,
+};
+
+constexpr uint32_t kNumHandLandmarks = static_cast<uint32_t>(HandLandmark::NUM_LANDMARKS);
+using Landmarks = std::array<Eigen::Vector3d, kNumHandLandmarks>;
+
+constexpr uint32_t kNumHandJointConnections = 23;
+constexpr std::array<std::pair<HandLandmark, HandLandmark>, kNumHandJointConnections>
+    kHandJointConnections = {
+        {{HandLandmark::WRIST, HandLandmark::PINKY_PROXIMAL},
+         {HandLandmark::PINKY_PROXIMAL, HandLandmark::PINKY_INTERMEDIATE},
+         {HandLandmark::PINKY_INTERMEDIATE, HandLandmark::PINKY_DISTAL},
+         {HandLandmark::PINKY_DISTAL, HandLandmark::PINKY_FINGERTIP},
+         {HandLandmark::WRIST, HandLandmark::RING_PROXIMAL},
+         {HandLandmark::RING_PROXIMAL, HandLandmark::RING_INTERMEDIATE},
+         {HandLandmark::RING_INTERMEDIATE, HandLandmark::RING_DISTAL},
+         {HandLandmark::RING_DISTAL, HandLandmark::RING_FINGERTIP},
+         {HandLandmark::WRIST, HandLandmark::MIDDLE_PROXIMAL},
+         {HandLandmark::MIDDLE_PROXIMAL, HandLandmark::MIDDLE_INTERMEDIATE},
+         {HandLandmark::MIDDLE_INTERMEDIATE, HandLandmark::MIDDLE_DISTAL},
+         {HandLandmark::MIDDLE_DISTAL, HandLandmark::MIDDLE_FINGERTIP},
+         {HandLandmark::WRIST, HandLandmark::INDEX_PROXIMAL},
+         {HandLandmark::INDEX_PROXIMAL, HandLandmark::INDEX_INTERMEDIATE},
+         {HandLandmark::INDEX_INTERMEDIATE, HandLandmark::INDEX_DISTAL},
+         {HandLandmark::INDEX_DISTAL, HandLandmark::INDEX_FINGERTIP},
+         {HandLandmark::WRIST, HandLandmark::THUMB_INTERMEDIATE},
+         {HandLandmark::THUMB_INTERMEDIATE, HandLandmark::THUMB_DISTAL},
+         {HandLandmark::THUMB_DISTAL, HandLandmark::THUMB_FINGERTIP},
+         {HandLandmark::THUMB_INTERMEDIATE, HandLandmark::INDEX_PROXIMAL},
+         {HandLandmark::INDEX_PROXIMAL, HandLandmark::MIDDLE_PROXIMAL},
+         {HandLandmark::MIDDLE_PROXIMAL, HandLandmark::RING_PROXIMAL},
+         {HandLandmark::RING_PROXIMAL, HandLandmark::PINKY_PROXIMAL}}};
+
 /**
  * @brief A struct representing wrist and palm tracking status per frame.
  */
 struct WristAndPalmPose {
   struct OneSide {
-    double confidence;
-    Eigen::Vector3d wristPosition_device;
-    Eigen::Vector3d palmPosition_device;
+    double confidence = 0.;
+    Eigen::Vector3d wristPosition_device{0., 0., 0.};
+    Eigen::Vector3d palmPosition_device{0., 0., 0.};
     struct WristAndPalmNormals {
-      Eigen::Vector3d palmNormal_device;
-      Eigen::Vector3d wristNormal_device;
+      Eigen::Vector3d palmNormal_device{0., 0., 0.};
+      Eigen::Vector3d wristNormal_device{0., 0., 0.};
     };
     std::optional<WristAndPalmNormals> wristAndPalmNormal_device;
   };
-  std::chrono::microseconds trackingTimestamp; /**< The timestamp of the wrist and palm tracking
-                                                  measurement in device time domain */
-  std::optional<OneSide> leftHand, rightHand; /**< The pose results of the
-                                                        wrist and palm tracking frame */
+  // The timestamp of the wrist and palm tracking measurement in device time domain.
+  std::chrono::microseconds trackingTimestamp{};
+  // Per-hand wrist and palm pose. Can be null if the hand is not tracked.
+  std::optional<OneSide> leftHand, rightHand;
 
   constexpr const std::optional<OneSide>& operator[](HANDEDNESS handedness) const {
-    if (handedness == HANDEDNESS::LEFT) {
-      return leftHand;
-    } else {
-      return rightHand;
+    return (handedness == HANDEDNESS::LEFT) ? leftHand : rightHand;
+  }
+};
+
+/**
+ * @brief A struct representing hand tracking result per frame.
+ */
+struct HandTrackingResult {
+  struct OneSide {
+    double confidence = 0.;
+    Landmarks landmarkPositions_device;
+    Sophus::SE3d T_Device_Wrist;
+    struct WristAndPalmNormals {
+      Eigen::Vector3d palmNormal_device{};
+      Eigen::Vector3d wristNormal_device{};
+    };
+    std::optional<WristAndPalmNormals> wristAndPalmNormal_device;
+
+    OneSide() {
+      landmarkPositions_device.fill(Eigen::Vector3d::Zero());
+      T_Device_Wrist = Sophus::SE3d();
     }
+
+    // Helper functions to get wrist/palm_positions_device.
+    [[nodiscard]] constexpr const Eigen::Vector3d& getWristPositionInDevice() const {
+      return landmarkPositions_device[static_cast<uint8_t>(HandLandmark::WRIST)];
+    }
+
+    [[nodiscard]] constexpr const Eigen::Vector3d& getPalmPositionInDevice() const {
+      return landmarkPositions_device[static_cast<uint8_t>(HandLandmark::PALM_CENTER)];
+    }
+  };
+
+  // The timestamp of the wrist and palm tracking measurement in device time domain.
+  std::chrono::microseconds trackingTimestamp{};
+
+  // Per-hand tracking result. Can be null if the hand is not tracked.
+  std::optional<OneSide> leftHand, rightHand;
+
+  constexpr const std::optional<OneSide>& operator[](HANDEDNESS handedness) const {
+    return (handedness == HANDEDNESS::LEFT) ? leftHand : rightHand;
   }
 };
 
@@ -61,4 +162,9 @@ struct WristAndPalmPose {
  * @brief alias to represent a vector of `WristAndPalmPose`
  */
 using WristAndPalmPoses = std::vector<WristAndPalmPose>;
+
+/**
+ * @brief alias to represent a vector of `HandTrackingResult`
+ */
+using HandTrackingResults = std::vector<HandTrackingResult>;
 } // namespace projectaria::tools::mps
