7c5ff6fdc7cf9622074fc9f5d58bc15d3204b402
Pierre Moulon <pierrem@meta.com>
2025-09-04 17:51:00 -0700

    {Build} Fixing Brew install CMake issue. (#256)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/projectaria_tools/pull/256
    
    This diff fixes the new brew install cmake issue on Github workflow. Installing a pre-installed package will trigger an error.
    
    Reviewed By: danielyan86129
    
    Differential Revision: D81616132
    
    fbshipit-source-id: 8729cd8c814334f0eeece25d79c0f9202ea432a9

--- cmake/Setup3rdParty.cmake
+++ cmake/Setup3rdParty.cmake
@@ -16,14 +16,14 @@ include(FetchContent) # once in the project to include the module
 
 FetchContent_Declare(
   Dispenso
-  GIT_REPOSITORY      https://github.com/facebookincubator/dispenso.git
-  GIT_TAG             f5d0dc5c5c02bf26063ab11ebfaa42f6d1a7c650 # v1.4.0
+  GIT_REPOSITORY https://github.com/facebookincubator/dispenso.git
+  GIT_TAG f5d0dc5c5c02bf26063ab11ebfaa42f6d1a7c650 # v1.4.0
 )
 
 FetchContent_Declare(
   vrs
-  GIT_REPOSITORY  https://github.com/facebookresearch/vrs.git
-  GIT_TAG        b5b854ea618f45fdce8c5d69e11cc879845adfc9 # master August 14, 2024.
+  GIT_REPOSITORY https://github.com/facebookresearch/vrs.git
+  GIT_TAG 26fdab54501d3567f27f953c13b9c786e000b7ee # master Sept 3, 2025.
 )
 # Override config for vrs
 option(UNIT_TESTS OFF)
@@ -35,13 +35,13 @@ FetchContent_MakeAvailable(vrs)
 
 FetchContent_Declare(
   eigen
-  GIT_REPOSITORY      https://gitlab.com/libeigen/eigen.git
-  GIT_TAG          19cacd3ecb9dab73c2dd7bc39d9193e06ba92bdd # 3.4.90
+  GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+  GIT_TAG 19cacd3ecb9dab73c2dd7bc39d9193e06ba92bdd # 3.4.90
 )
 FetchContent_Declare(
   Sophus
-  GIT_REPOSITORY      https://github.com/strasdat/Sophus.git
-  GIT_TAG          de0f8d3d92bf776271e16de56d1803940ebccab9 # 1.22.10
+  GIT_REPOSITORY https://github.com/strasdat/Sophus.git
+  GIT_TAG de0f8d3d92bf776271e16de56d1803940ebccab9 # 1.22.10
 )
 
 # Override config for Sophus
@@ -52,14 +52,14 @@ add_definitions("-DSOPHUS_DISABLE_ENSURES")
 FetchContent_Declare(
   cli11
   GIT_REPOSITORY https://github.com/CLIUtils/CLI11
-  GIT_TAG        v2.4.2
+  GIT_TAG v2.4.2
 )
 
 
 FetchContent_Declare(
   nlohmann-json
   GIT_REPOSITORY https://github.com/nlohmann/json.git
-  GIT_TAG        v3.11.3
+  GIT_TAG v3.11.3
 )
 
 set(dependencies cli11 eigen Sophus Dispenso nlohmann-json)
@@ -72,8 +72,8 @@ include(ExternalProject)
 ExternalProject_Add(
   fast-cpp-csv-parser
   GIT_REPOSITORY https://github.com/ben-strasser/fast-cpp-csv-parser.git
-  GIT_TAG        origin/master
-  SOURCE_DIR     "${CMAKE_BINARY_DIR}/_deps/fast-cpp-csv-parser"
+  GIT_TAG origin/master
+  SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/fast-cpp-csv-parser"
   # disable following, since it is not needed
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
